93936f5b699d586da026497e1e20f950064dde82
jatinsonwal01 <jatinsonwal1001@gmail.com>
2023-01-17 23:50:15 +0530

    init commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require("./db/mongoose");
+const express = require("express");
+const User = require("./models/user");
+const Task = require("./models/task");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+app.listen(port, () => {
+  console.log("server is up on port" + port);
+});
